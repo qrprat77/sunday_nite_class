0ff7bff7fb39b9fae9f024b71b22708dac079bd6
G. Brandon Hoyt <qrprat77@CodeMonkey.(none)>
2009-10-25 17:56:20 -0400

    mindmap

---
+++ youngsingles.mm
@@ -0,0 +1,61 @@
+<map version="0.7.1">
+<node COLOR="#ff0000" TEXT="young singles" FOLDED="true">
+<font NAME="Sindbad" SIZE="28"/>
+<node TEXT="advertise new crowd" FOLDED="true" POSITION="right">
+<font NAME="SansSerif" SIZE="20"/>
+<node TEXT="New Kids in Church"/>
+<node TEXT="People we&apos;ll meet at skool."/>
+<node TEXT="Young singles that we work with."/>
+</node>
+<node COLOR="#000099" TEXT="goals and planning" FOLDED="true" POSITION="right">
+<edge COLOR="#009999" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="20"/>
+<icon BUILTIN="messagebox_warning"/>
+<node TEXT="We want to see 10 new faces in class." FOLDED="true">
+<font NAME="SansSerif" SIZE="28"/>
+<icon BUILTIN="flag"/>
+<node TEXT="invite to a special service?">
+<node TEXT="retreat weekend"/>
+<node TEXT="Special events"/>
+</node>
+<node TEXT="Unbound Bible Study.">
+<icon BUILTIN="clanbomber"/>
+<node TEXT="time:"/>
+<node TEXT="subject: 3 question lessons"/>
+<node TEXT="location: "/>
+</node>
+</node>
+<node TEXT="Replan some trips." FOLDED="true">
+<font NAME="SansSerif" SIZE="28"/>
+<icon BUILTIN="pencil"/>
+<node TEXT="nursing homes?">
+<font NAME="SansSerif" SIZE="28"/>
+</node>
+<node TEXT="Church member specific outreach?">
+<font NAME="SansSerif" SIZE="28"/>
+</node>
+<node TEXT="Talbot/lightouse ministries?">
+<font NAME="SansSerif" SIZE="28"/>
+</node>
+</node>
+<node TEXT="Community outreach." FOLDED="true">
+<font NAME="SansSerif" SIZE="28"/>
+<icon BUILTIN="pencil"/>
+<node TEXT="Water round the lake"/>
+<node TEXT="mid term special"/>
+<node TEXT="Finals week special event."/>
+</node>
+</node>
+<node COLOR="#000099" TEXT="New Name?" FOLDED="true" POSITION="right">
+<edge COLOR="#009999" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="20"/>
+<icon BUILTIN="bookmark"/>
+<node TEXT="&apos;CYP&apos;">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node TEXT="???">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
